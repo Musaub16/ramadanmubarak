7dc04e9462f8533996b78162a1575837ba5b472f
Darda <dardausa@gmail.com>
2021-04-14 15:08:24 -0500

    first commit

---
+++ src/com/ramadanmubarak/Read.java
@@ -0,0 +1,16 @@
+package com.ramadanmubarak;
+
+public class Read {
+
+	public static void main(String[] args) {
+		
+		readingQuran();
+
+	}
+	
+	public static void readingQuran() {
+		
+		System.out.println("We are reading Quran in Ramadan month.....");
+	}
+
+}
